e80f05591c2d497fbbd658950448d6c5c6e56efc
Billcountry <me@billcountry.tech>
2020-02-02 16:39:18 +0300

    New Build

---
+++ precache-manifest.7a3ad2ccae8a3c4a6f303cbb5ab50922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d6b6921e054899317c85823780d308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "619d3e9d783b5a9cfd3b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c60fabb825023ee8903",
+    "url": "/static/js/2.5c202fd7.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.5c202fd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619d3e9d783b5a9cfd3b",
+    "url": "/static/js/main.a075885a.chunk.js"
+  },
+  {
+    "revision": "2605cdf94f541ac437a1",
+    "url": "/static/js/runtime-main.e29ab44a.js"
+  },
+  {
+    "revision": "19480d7eb86720c53b3f4f252bd2ce43",
+    "url": "/static/media/logo.19480d7e.jpg"
+  }
+]);
